f6aa45dafee90b9cb218a09cf324a556f0d7c8d3
vab2410tb-ui <vab2410tb@gmail.com>
2026-03-19 20:48:09 +0700

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// Import 2 hàm bạn đã viết bên controller
+
 import { requestOTP, verifyOTP, updateUserProfile, getUserProfile } from '../app/controllers/auth.controller.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
